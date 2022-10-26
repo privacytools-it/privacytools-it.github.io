7ec81c681df0e27f37e347791f010e89e626097a
privacytools-it <czvetkova.ira.2018@mail.ru>
2022-10-26 12:00:19 +0300

    init

---
+++ assets/js/applytheme.js
@@ -0,0 +1,7 @@
+if (localStorage.getItem("colorScheme") === "dark") {
+	document.querySelector("#dark-css").removeAttribute("media"); // Set dark theme
+}
+
+else if (localStorage.getItem("colorScheme") === "light") {
+	document.querySelector("#dark-css").setAttribute("media", "invalid"); // Set light theme
+}
